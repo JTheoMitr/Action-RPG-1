75578189597d84de081aab08f01d30eb95e2d3c0
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2024-01-13 00:01:08 -0600

    various two
    a few more smol changes to the droids

--- Enemies/Droid.gd
+++ Enemies/Droid.gd
@@ -80,11 +80,11 @@ func seek_player():
 		get_parent().add_child(droidSound)
 		droidSound.play(0.0)
 		timer.start(0.0)
-		if laserEngaged == false:
-			var laser = Laser.instance()
-			laser.global_position = global_position
-			get_parent().call_deferred("add_child", laser)
-			laserEngaged = true
+		# if laserEngaged == false:
+			# var laser = Laser.instance()
+			# laser.global_position = global_position
+			# get_parent().call_deferred("add_child", laser)
+			# laserEngaged = true
 		state = CHASE
 		
 
