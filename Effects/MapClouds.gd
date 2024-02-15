6bf13103af7253f46f7f7c068636ed96785d6bd1
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2024-02-15 00:38:33 -0600

    effects randomization
    added random start frame to map cloud and smoke stack effect

---
+++ Effects/MapClouds.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.frame = random_drop_generator([0, 1, 2])
+
+func random_drop_generator(drop_list):
+	drop_list.shuffle()
+	return drop_list.pop_front()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
