8c7b7fa647db32900a19e55348c51bd474660f36
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2024-02-26 23:43:49 -0600

    map and world 1
    updated world 1 background sprite and labels in world map

--- UI/WorldMapScreen.gd
+++ UI/WorldMapScreen.gd
@@ -8,6 +8,7 @@ onready var cityButton = $CityButton
 onready var desertButton = $DesertButton
 onready var blizzardButton = $BlizzardButton
 onready var levelText = $RichTextLabel
+onready var levelPreText = $RichTextLabel2
 onready var selectSound = $SelectSound
 onready var mapMusic = $MapMusic
 onready var timer = $Timer
@@ -31,27 +32,33 @@ func _process(delta):
 	if forestButton.has_focus():
 		wolfIcon.position.x = 191
 		wolfIcon.position.y = 159
-		levelText.text = "The \n Forest"
+		levelPreText.text = "The"
+		levelText.text = "Forest"
 	if caveButton.has_focus():
 		wolfIcon.position.x = 171
 		wolfIcon.position.y = 142
-		levelText.text = "Wylde \n Caverns"
+		levelPreText.text = "Wylde"
+		levelText.text = "Caverns"
 	if marshButton.has_focus():
 		wolfIcon.position.x = 183
 		wolfIcon.position.y = 112
-		levelText.text = "Toad \n Marsh"
+		levelPreText.text = "Toad"
+		levelText.text = "Marsh"
 	if cityButton.has_focus():
 		wolfIcon.position.x = 174
 		wolfIcon.position.y = 66
-		levelText.text = "Arcade \n Heights"
+		levelPreText.text = "Arcade"
+		levelText.text = "Heights"
 	if desertButton.has_focus():
 		wolfIcon.position.x = 128
 		wolfIcon.position.y = 85
-		levelText.text = "The \n Desert"
+		levelPreText.text = "The"
+		levelText.text = "Desert"
 	if blizzardButton.has_focus():
 		wolfIcon.position.x = 152
 		wolfIcon.position.y = 34
-		levelText.text = "Bliz \n Canyon"
+		levelPreText.text = "Blizzard"
+		levelText.text = "Canyon"
 		
 		
 
