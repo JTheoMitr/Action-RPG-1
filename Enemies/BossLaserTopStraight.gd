8a0001d0b4796a7562b1025dd5e6a41df80fd319
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2024-03-13 02:44:46 -0500

    new lasers
    four new boss lasers for NWSE

---
+++ Enemies/BossLaserTopStraight.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var velocity = Vector2.ZERO
+onready var timer = $Timer
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.global_position.y -= 1
+
+
+func _on_Timer_timeout():
+	queue_free()
