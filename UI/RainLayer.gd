99bae42eb19e475ce7ecdb176bb71da7495a4911
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2024-05-14 14:14:10 -0500

    rain UI updates
    fading dark skies and timer work

--- UI/RainLayer.gd
+++ UI/RainLayer.gd
@@ -7,17 +7,44 @@ extends Control
 onready var darkSky = $DarkSkyLayer
 onready var rain = $Rain
 onready var lightning = $Lightning
+onready var lightning2 = $Lightning2
 onready var rainTimer = $RainTimer
 onready var rainStopTimer = $RainStopTimer
-onready var thunder = $Thunder
+onready var thunderSound = $Thunder
+onready var thunderSound2 = $Thunder2
 onready var rainSound = $RainSound
+onready var lightningTimer = $LightningTimer
+onready var thunderTimer = $ThunderTimer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	rainTimer.wait_time = random_drop_generator([15, 20, 25])
+	rainTimer.start(0.0)
+	lightning.hide()
+	lightning2.hide()
+	rain.hide()
+	darkSky.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func random_drop_generator(drop_list):
+	drop_list.shuffle()
+	return drop_list.pop_front()
+
+func _on_RainTimer_timeout():
+	var thunder = random_drop_generator([thunderSound, thunderSound2])
+	rainStopTimer.wait_time = random_drop_generator([15, 30, 45])
+	$Tween.interpolate_property(darkSky, "color", Color(0, 0, 0, 0), Color(0, 0, 0, 0.3), 2, Tween.TRANS_LINEAR)
+	$Tween.start()
+	rainSound.play(0.0)
+	thunder.play(0.0)
+	darkSky.show()
+	rain.show()
+
+
+func _on_RainSound_finished():
+	rainSound.play(0.0)
