81d37dce275d0524b92e6e2027700058c1899512
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2024-07-16 00:57:03 -0500

    more xp ui updates
    jury's still out on whether this is our final

--- UI/XpUI.gd
+++ UI/XpUI.gd
@@ -7,13 +7,15 @@ onready var animSprite = $AnimatedSprite
 func _process(delta):
 	$RichTextLabel.text = "xp " + str(stats.xp) + " / " + str(stats.xpCap)
 	
-	if stats.xp < (stats.xpCap * .2):
-		animSprite.frame = 4
-	if stats.xp < (stats.xpCap * .4) && stats.xp > (stats.xpCap * .2):
+	if stats.xp < (stats.xpCap * .18):
+		animSprite.frame = 5
+	if stats.xp < (stats.xpCap * .35) && stats.xp > (stats.xpCap * .15):
+		animSprite.frame = 4	
+	if stats.xp < (stats.xpCap * .6) && stats.xp > (stats.xpCap * .35):
 		animSprite.frame = 3	
-	if stats.xp < (stats.xpCap * .65) && stats.xp > (stats.xpCap * .4):
+	if stats.xp < (stats.xpCap * .80) && stats.xp > (stats.xpCap * .6):
 		animSprite.frame = 2	
-	if stats.xp < (stats.xpCap * .85) && stats.xp > (stats.xpCap * .65):
+	if stats.xp < (stats.xpCap * .95) && stats.xp > (stats.xpCap * .80):
 		animSprite.frame = 1	
 	if stats.xp > (stats.xpCap * .85):
 		animSprite.frame = 0
