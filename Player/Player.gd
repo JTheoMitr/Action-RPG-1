8a9205cfed3181a6b0ca4fa4bcc9f88eb31fb79c
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2024-07-22 21:33:14 -0500

    sword meter
    charge attack meter added

--- Player/Player.gd
+++ Player/Player.gd
@@ -209,6 +209,7 @@ func move_state(delta):
 		if Input.is_action_just_released("attack"):
 			if chargeReady == true:
 				playerSpriteSpecials.show()
+				hurtbox.monitoring = false
 				aura.show()
 				aura.play("default")
 				state = ATTACK
@@ -363,6 +364,7 @@ func bad_trippin():
 func sword_special_over():
 	#blastZone.disabled = true
 	self.MAX_SPEED = 90
+	hurtbox.monitoring = true
 	veLockity = false
 	playerSpriteSpecials.hide()
 	aura.hide()
--- UI/XpUI.gd
+++ UI/XpUI.gd
@@ -3,23 +3,40 @@ extends Control
 
 onready var stats = PlayerStats
 onready var animSprite = $AnimatedSprite
+var chargeTrue = false
 
 func _process(delta):
 	$RichTextLabel.text = "xp " + str(stats.xp) + " / " + str(stats.xpCap)
 	
-	if stats.xp < (stats.xpCap * .18):
-		animSprite.frame = 5
-	if stats.xp < (stats.xpCap * .35) && stats.xp > (stats.xpCap * .15):
-		animSprite.frame = 4	
-	if stats.xp < (stats.xpCap * .6) && stats.xp > (stats.xpCap * .35):
-		animSprite.frame = 3	
-	if stats.xp < (stats.xpCap * .80) && stats.xp > (stats.xpCap * .6):
-		animSprite.frame = 2	
-	if stats.xp < (stats.xpCap * .95) && stats.xp > (stats.xpCap * .80):
-		animSprite.frame = 1	
-	if stats.xp > (stats.xpCap * .85):
-		animSprite.frame = 0
+	if Input.is_action_just_pressed("attack"):
+		if chargeTrue == false:
+			animSprite.play()
 	
+	if Input.is_action_just_released("attack"):
+			chargeTrue = false
+			animSprite.stop()
+			animSprite.frame = 0
+			
+#	if stats.xp < (stats.xpCap * .18):
+#		animSprite.frame = 5
+#	if stats.xp < (stats.xpCap * .35) && stats.xp > (stats.xpCap * .15):
+#		animSprite.frame = 4	
+#	if stats.xp < (stats.xpCap * .6) && stats.xp > (stats.xpCap * .35):
+#		animSprite.frame = 3	
+#	if stats.xp < (stats.xpCap * .80) && stats.xp > (stats.xpCap * .6):
+#		animSprite.frame = 2	
+#	if stats.xp < (stats.xpCap * .95) && stats.xp > (stats.xpCap * .80):
+#		animSprite.frame = 1	
+#	if stats.xp > (stats.xpCap * .85):
+#		animSprite.frame = 0
+#
 func _ready():
 	$RichTextLabel.text = "xp " + str(stats.xp) + " / " + str(stats.xpCap)
+	animSprite.frame = 0
 	
+
+
+func _on_AnimatedSprite_animation_finished():
+	chargeTrue = true
+	animSprite.frame = 7
+	animSprite.stop()
