4fe603ea63f1aeb3a9a19c87535bb63885f5e334
John Theo Mitropoulos <70192782+JTheoMitr@users.noreply.github.com>
2024-07-27 17:35:19 -0500

    updated chill willy flow
    slightly smootha

--- World/Market.gd
+++ World/Market.gd
@@ -3,8 +3,11 @@ extends KinematicBody2D
 const MarketInventory = preload("res://Inventory/MarketInventory.tscn")
 const MarketSound = preload("res://Music and Sounds/MarketSoundOne.tscn")
 
-onready var popup = $PopupDialog
-onready var textLabel = $PopupDialog/RichTextLabel
+onready var popup = $CanvasLayer/PopupDialog
+onready var textLabel = $CanvasLayer/PopupDialog/RichTextLabel
+onready var panel = $CanvasLayer/Panel
+onready var sprite2 = $CanvasLayer/PopupDialog/Sprite2
+onready var label = $CanvasLayer/PopupDialog/Label
 
 
 var inArea = false
@@ -13,17 +16,19 @@ var textOptions = ["[center] Hey, my dude... you lookin' to \n get down on some 
 func _ready():
 	textLabel.bbcode_enabled = true
 	textLabel.bbcode_text = textOptions[0]
+	panel.hide()
 
 func _process(delta):
 	if (inArea == true) && (textLabel.bbcode_text == textOptions[0]):
+		panel.show()
 		popup.show() 
 		PlayerStats.emit_signal("player_paused")
 		textLabel.bbcode_text = textOptions[0]
-	popup.rect_global_position.x = self.position.x - 30
-	popup.rect_global_position.y = self.position.y + 40
 	if (Input.is_action_pressed("interact")) && (inArea == true):
 		if textLabel.bbcode_text == textOptions[0]:
 			textLabel.bbcode_text = textOptions[1]
+			sprite2.hide()
+			label.hide()
 			$Timer.start()
 	
 
@@ -31,7 +36,7 @@ func show_market_inventory():
 	var marketInventory = MarketInventory.instance()
 	var world = get_tree().current_scene
 	get_parent().add_child(marketInventory)
-	
+	panel.hide()
 	popup.hide()
 	marketInventory.global_position.x = self.position.x - 55
 	marketInventory.global_position.y = self.position.y
@@ -46,6 +51,8 @@ func _on_Area2D_area_entered(area):
 
 func _on_Area2D_area_exited(area):
 	inArea = false
+	sprite2.show()
+	label.show()
 	textLabel.bbcode_text = textOptions[0]
 	popup.hide()
 	
