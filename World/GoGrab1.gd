be1eeeb4392d55b4387ae9ef0f6af3ba2dbad4e1
JTheoMitr <johnmittz1@gmail.com>
2025-05-16 00:25:24 -0500

    gograb updates 2
    finished *enough for demo

--- World/GoGrab1.gd
+++ World/GoGrab1.gd
@@ -5,12 +5,19 @@ extends Node2D
 # var a = 2
 # var b = "text"
 onready var mainButton = $HBoxContainer/Button
+onready var music = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mainButton.grab_focus()
+	music.play(0.0)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_AudioStreamPlayer_finished():
+	music.play(0.0)
