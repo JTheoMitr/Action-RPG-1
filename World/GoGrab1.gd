a5289900fd5b07657f628ad564b7b499508da6f7
JTheoMitr <johnmittz1@gmail.com>
2025-05-16 01:34:12 -0500

    gograb updates 3
    functional buttons and popup started

--- World/GoGrab1.gd
+++ World/GoGrab1.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-
+const CashSound = preload("res://Music and Sounds/CashRegisterSound.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 onready var mainButton = $HBoxContainer/Button
 onready var music = $AudioStreamPlayer
+onready var popup = $PopupDialog
+onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,3 +23,19 @@ func _ready():
 
 func _on_AudioStreamPlayer_finished():
 	music.play(0.0)
+
+
+func _on_Button_pressed():
+	var cashSound = CashSound.instance()
+	get_tree().current_scene.add_child(cashSound)
+	popup.popup()
+	
+	#set up health increase, folow suit for other buttons
+
+
+func _on_PopupDialog_about_to_show():
+	timer.start()
+
+
+func _on_Timer_timeout():
+	popup.hide()
